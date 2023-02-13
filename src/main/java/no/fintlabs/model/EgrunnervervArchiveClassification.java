8b4d27547df3e648c3bd4a0f39fc3e7ef6652284
Nils H Opsahl <nils.hakon.opsahl@vigoiks.no>
2023-02-13 09:30:47 +0100

    Changes for use of new model

--- src/main/java/no/fintlabs/model/EgrunnervervArchiveClassification.java
+++ src/main/java/no/fintlabs/model/EgrunnervervArchiveClassification.java
@@ -5,14 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class EgrunnervervArchiveClassification {
+    @NotNull
     private String ordningsprinsipp;
+    @NotNull
     private String ordningsverdi;
+    @NotNull
     private String beskrivelse;
+    @NotNull
     private String sortering;
+    @NotNull
     private String untattOffentlighet;
 }

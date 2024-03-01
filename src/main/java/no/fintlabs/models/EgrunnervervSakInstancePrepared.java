8cfd69bdca8991b57d2416b4f9ced635d712c264
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-03-01 14:51:19 +0100

    fix validation for JournalpostInstanceBody and add valdiation for DokumentBase64 and remove unneeded validation (#35)

--- src/main/java/no/fintlabs/models/EgrunnervervSakInstancePrepared.java
+++ src/main/java/no/fintlabs/models/EgrunnervervSakInstancePrepared.java
@@ -6,9 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.jackson.Jacksonized;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
@@ -17,40 +14,24 @@ import java.util.List;
 @Builder
 public class EgrunnervervSakInstancePrepared {
 
-    @NotBlank
     private final String sysId;
-    @NotNull
     private final String knr;
-    @NotNull
     private final String gnr;
-    @NotNull
     private final String bnr;
-    @NotNull
     private final String fnr;
-    @NotNull
     private final String snr;
-    @NotNull
     private final String takstnummer;
-    @NotNull
     private final String tittel;
-    @NotNull
     private final String saksansvarligEpost;
-    @NotNull
     @Setter
     private String saksansvarlig;
-    @NotNull
     private final String eierforholdsnavn;
-    @NotNull
     private final String eierforholdskode;
-    @NotNull
     private final String prosjektnr;
-    @NotNull
     private final String prosjektnavn;
-    @NotNull
     private final String kommunenavn;
-    @NotNull
     private final String adresse;
 
-    private final List<@Valid @NotNull EgrunnervervSaksPart> saksparter;
-    private final List<@Valid @NotNull EgrunnervervSakKlassering> klasseringer;
+    private final List<EgrunnervervSaksPart> saksparter;
+    private final List<EgrunnervervSakKlassering> klasseringer;
 }
